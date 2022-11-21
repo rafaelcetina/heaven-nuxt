69bbd53bff776b96546168affb9243b15a4a57fb
Rodolfo Rafael Dzul Cetina <rdzul@palaceresorts.com>
2022-11-20 19:10:08 -0600

    added favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,22 @@ import { useI18n } from "vue-i18n";
 const { t } = useI18n();
 
 useHead({
-  title: t("app_name"),
+  title: "Jebem Bienes raices",
+  meta: [
+    {
+      htmlAttrs: {
+        lang: "es",
+      },
+    },
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    {
+      hid: "Compra venta y renta de bienes raices en Calakmul",
+      name: "Jebem bienes raices",
+      content: "Compra venta y renta de bienes raices en Calakmul",
+    },
+  ],
+  link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
 });
 
 const route = useRoute();
