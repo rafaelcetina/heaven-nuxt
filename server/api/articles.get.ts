2138484a3bd4367772ea440c00e730495dc94166
Rodolfo Rafael Dzul Cetina <rdzul@palaceresorts.com>
2022-12-16 12:21:31 -0600

    Update articles.get.ts

--- server/api/articles.get.ts
+++ server/api/articles.get.ts
@@ -105,7 +105,7 @@ export default defineEventHandler(() => {
         body: "<strong>Precio: </strong> $650,000 MXN <br> Dos cuartos, un baño, sala y cocina completa",
         createdAt: "2022-11-07T22:07:44.308Z",
         updatedAt: "2022-11-13T03:57:54.051Z",
-        publishedAt: "2022-12-07T22:10:22.775Z",
+        publishedAt: "2022-12-16T22:10:22.775Z",
         locale: "es-MX",
         image: {
           data: {
@@ -190,7 +190,7 @@ export default defineEventHandler(() => {
       features: {
         id: 2, // importante
         name: "Casa en venta",
-        description: "Casa en venta. Xpujil Calakml, 388.25 m2. ¡Aprovecha!",
+        description: "Casa en venta. Xpujil Calakmul, 388.25 m2. ¡Aprovecha!",
         image:
           "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/casa-azul/3.jpeg",
         imagePosition: "right",
@@ -210,6 +210,230 @@ export default defineEventHandler(() => {
           {
             title: "Precio de venta",
             description: "$650,000 MXN",
+          },
+          {
+            title: "Contácto",
+            description: "983 332 1375",
+          },
+        ],
+      },
+    },
+    {
+      id: 3,
+      attributes: {
+        title: "¡Oportunidad! Terreno para construcción",
+        excerpt: "Precio: $1,000,000 MXN",
+        slug: "terreno-1h",
+        body: "<strong>Precio: </strong> $1,000,000 MXN <br> Terreno valdio, 100m x 100m",
+        createdAt: "2022-11-07T22:07:44.308Z",
+        updatedAt: "2022-11-13T03:57:54.051Z",
+        publishedAt: "2022-12-07T22:10:22.775Z",
+        locale: "es-MX",
+        image: {
+          data: {
+            attributes: {
+              name: "terreno-1h-1.jpeg",
+              alternativeText: "terreno-1h-1.jpeg",
+              formats: {
+                thumbnail: {
+                  url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-1h/1_tn.jpg",
+                },
+              },
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-1h/1.jpeg",
+            },
+          },
+        },
+      },
+      gallery: [
+        {
+          data: {
+            attributes: {
+              name: "terreno-1h-1.jpeg",
+              alternativeText: "terreno-1h-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-1h/1.jpeg",
+            },
+          },
+        },
+        {
+          data: {
+            attributes: {
+              name: "terreno-1h-1.jpeg",
+              alternativeText: "terreno-1h-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-1h/2.jpeg",
+            },
+          },
+        },
+        {
+          data: {
+            attributes: {
+              name: "terreno-1h-1.jpeg",
+              alternativeText: "terreno-1h-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-1h/3.jpeg",
+            },
+          },
+        },
+        {
+          data: {
+            attributes: {
+              name: "terreno-1h-1.jpeg",
+              alternativeText: "terreno-1h-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-1h/4.jpeg",
+            },
+          },
+        },
+        {
+          data: {
+            attributes: {
+              name: "terreno-1h-1.jpeg",
+              alternativeText: "terreno-1h-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-1h/5.jpeg",
+            },
+          },
+        },
+        {
+          data: {
+            attributes: {
+              name: "terreno-1h-1.jpeg",
+              alternativeText: "terreno-1h-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-1h/6.jpeg",
+            },
+          },
+        },
+      ],
+      features: {
+        id: 3, // importante
+        name: "Terreno para construcción",
+        description: "Terreno en venta. Xpujil Calakmul, 1,000 m2. ¡Aprovecha!",
+        image:
+          "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-1h/3.jpeg",
+        imagePosition: "right",
+        imageAlt: "Logo Jebem",
+        theme: "", // default
+        items: [
+          {
+            title: "Descripción",
+            description:
+              "oportunidad para construir hotel, tiendas de convenencia, farmacias o bancos",
+          },
+          {
+            title: "Dirección",
+            description:
+              "Av. Nadscan entre calle Pechal colonia Jardines. Xpujil, Calakmul. Campeche. CP: 24640",
+          },
+          {
+            title: "Precio de venta",
+            description: "$1,000,000 MXN",
+          },
+          {
+            title: "Contácto",
+            description: "983 332 1375",
+          },
+        ],
+      },
+    },
+    {
+      id: 4,
+      attributes: {
+        title: "¡Oportunidad para centro comercial! Terreno",
+        excerpt: "Precio: $7,500,000 MXN",
+        slug: "terreno-hh",
+        body: "<strong>Precio: </strong> $7,500,000 MXN <br> Terreno con excelente ubicación",
+        createdAt: "2022-11-07T22:07:44.308Z",
+        updatedAt: "2022-11-13T03:57:54.051Z",
+        publishedAt: "2022-12-16T22:10:22.775Z",
+        locale: "es-MX",
+        image: {
+          data: {
+            attributes: {
+              name: "terreno-hh-1.jpeg",
+              alternativeText: "terreno-hh-1.jpeg",
+              formats: {
+                thumbnail: {
+                  url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-hh/1_tn.jpg",
+                },
+              },
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-hh/1.jpeg",
+            },
+          },
+        },
+      },
+      gallery: [
+        {
+          data: {
+            attributes: {
+              name: "terreno-hh-1.jpeg",
+              alternativeText: "terreno-hh-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-hh/1.jpeg",
+            },
+          },
+        },
+        {
+          data: {
+            attributes: {
+              name: "terreno-hh-1.jpeg",
+              alternativeText: "terreno-hh-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-hh/2.jpeg",
+            },
+          },
+        },
+        {
+          data: {
+            attributes: {
+              name: "terreno-hh-1.jpeg",
+              alternativeText: "terreno-hh-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-hh/3.jpeg",
+            },
+          },
+        },
+        {
+          data: {
+            attributes: {
+              name: "terreno-hh-1.jpeg",
+              alternativeText: "terreno-hh-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-hh/4.jpeg",
+            },
+          },
+        },
+        {
+          data: {
+            attributes: {
+              name: "terreno-hh-1.jpeg",
+              alternativeText: "terreno-hh-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-hh/5.jpeg",
+            },
+          },
+        },
+        {
+          data: {
+            attributes: {
+              name: "terreno-hh-1.jpeg",
+              alternativeText: "terreno-hh-1.jpeg",
+              url: "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-hh/6.jpeg",
+            },
+          },
+        },
+      ],
+      features: {
+        id: 4, // importante
+        name: "Oportunidad para centro comercial",
+        description: "Terreno en venta. Xpujil Calakmul. ¡Aprovecha!",
+        image:
+          "https://qzxcfcpyemrhuakuymvj.supabase.co/storage/v1/object/public/jebem/terreno-hh/3.jpeg",
+        imagePosition: "right",
+        imageAlt: "Logo Jebem",
+        theme: "", // default
+        items: [
+          {
+            title: "Descripción",
+            description: "oportunidad para construir centro comercial",
+          },
+          {
+            title: "Dirección",
+            description: "Xpujil, Calakmul. Campeche. CP: 24640",
+          },
+          {
+            title: "Precio de venta",
+            description: "$1,000,000 MXN",
           },
           {
             title: "Contácto",
